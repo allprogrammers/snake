3b0e239bbd389834dfe3938201731f5657ccd6bf
M.Hamza Ali <hamzaali44@gmail.com>
2017-06-05 00:19:43 +0500

    canvas

--- snake.py
+++ snake.py
@@ -1,4 +1,9 @@
-from tkinter import Tk
+from tkinter import *
 
 screen = Tk()
+canvas = Canvas(screen,width=100, height=100)
+canvas.pack()
+oneline = canvas.create_line(0,0,50,50)
+twoline = canvas.create_line(0,0,24,50,fill="red")
+canvas.delete(oneline)
 screen.mainloop()
