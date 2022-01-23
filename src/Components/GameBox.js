8e4f5a6c393ece9fb08f09722f50555d8ee1ce16
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-01-23 23:47:08 +0100

    init GameBox

---
+++ src/Components/GameBox.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Styles/GameBox.css"
+
+const GameBox = ()=>{
+    return (<div className="parentGameBox"><div className="GameBox">asdf</div></div>)
+}
+
+export default GameBox;
