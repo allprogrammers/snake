0b7af1bfc33d77359e93a77e0902c431d3acc65f
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-01-24 16:42:51 +0100

    Emptied the cells

--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -5,7 +5,7 @@ const Cell = (props) => {
     let liClasses = [Styles.Cell]
     liClasses.push(props.activated?Styles.active:Styles.inactive)
 
-    return (<div onClick={props.onClick}className={liClasses.join(" ")}>{props.rown},{props.coln}</div>)
+    return (<div onClick={props.onClick}className={liClasses.join(" ")}></div>)
 }
 
 export default Cell;
