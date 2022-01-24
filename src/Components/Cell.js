b4ea825ededc8e8aed6bb6afc81fcd470a569f2a
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-01-24 10:13:32 +0100

    Added Cell and OnClick event for cells

---
+++ src/Components/Cell.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Styles from "./Styles/Cell.module.css"
+
+const Cell = (props) => {
+    let liClasses = [Styles.Cell]
+    liClasses.push(props.activated?Styles.active:Styles.inactive)
+
+    return (<div onClick={props.onClick}className={liClasses.join(" ")}>{props.rown},{props.coln}</div>)
+}
+
+export default Cell;
--- src/Components/GameBox.js
+++ src/Components/GameBox.js
@@ -1,8 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
+
+import Cell from "./Cell"
+
 import Styles from"./Styles/GameBox.module.css"
 
 const GameBox = ()=>{
-    return (<div className={Styles.GameBox}></div>)
+    //10x10 grid
+    const [snake,setSnake] = useState([...Array(10)].map(x=>[...Array(10)].map(y=>0)))
+    
+
+    const handleClick= (row,col)=>{
+        setSnake(snake.map((x,i)=> {
+            if(i==row)
+            {
+                x[col]=x[col]?0:1;
+            }
+            return x;
+        }));
+    }
+
+    return (
+    <div className={Styles.GameBox}>
+        {[...Array(10)].map((x,row)=>
+        (
+            <div key={row} className={Styles.GameBoxColumn}>
+                {[...Array(10)].map((y,col)=> 
+                (
+                    <Cell onClick={function(){handleClick(row,col)}} rown={row} coln={col} activated={snake[row][col]}key={col}/>
+                ))}
+            </div>
+        ))}
+    </div>
+    )
 }
 
 export default GameBox;
