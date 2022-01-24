e079d5feef76176530c9a1dd5ac71f16a1b330eb
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-01-24 16:43:51 +0100

    Added motion to Snake on Board

--- src/Components/GameBox.js
+++ src/Components/GameBox.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import Cell from "./Cell"
 
@@ -9,9 +9,34 @@ const height = 10;
 
 const GameBox = ()=>{
     //10x10 grid
-    const [board,setBoard] = useState([...Array(height)].map(x=>[...Array(width)].map(y=>0)))
-    
-    
+    const [snake,setSnake] = useState([[0,0]])
+    const [board,setBoard] = useState([...Array(height)].map((row,rowi)=> [...Array(width)].map((col,coli)=>0)))
+    const [xdir,setxdir] = useState(1)
+    const [ydir,setydir] = useState(0)
+
+    useEffect(()=>{
+        const intervalID = setInterval(() => {
+            setSnake((oldSnake) => {
+                var last = oldSnake[oldSnake.length-1];
+                var nextStep = [last[0]+ydir,last[1]+xdir];
+                var newSnake = [...oldSnake]
+                newSnake.push(nextStep);
+                newSnake.shift();
+                return newSnake;
+            })
+        }, 500);
+        return ()=>{clearInterval(intervalID)}
+    },[])
+
+    useEffect(()=>{
+        setBoard(
+            [...Array(height)].map((row,rowi)=> 
+                [...Array(width)].map((col,coli)=>
+                    snake.filter(pos => (pos[0]===rowi && pos[1]===coli)).length===0?0:1)
+                )
+            )
+
+    },[snake])
 
     const handleClick= (row,col)=>{
         setBoard(board.map((x,i)=> {
