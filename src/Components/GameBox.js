e040484c557978144789555d3ef6ffb8a88a7721
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-01-24 10:41:04 +0100

    extracted width and height and separated snake and board

--- src/Components/GameBox.js
+++ src/Components/GameBox.js
@@ -4,13 +4,17 @@ import Cell from "./Cell"
 
 import Styles from"./Styles/GameBox.module.css"
 
+const width = 10;
+const height = 10;
+
 const GameBox = ()=>{
     //10x10 grid
-    const [snake,setSnake] = useState([...Array(10)].map(x=>[...Array(10)].map(y=>0)))
+    const [board,setBoard] = useState([...Array(height)].map(x=>[...Array(width)].map(y=>0)))
+    
     
 
     const handleClick= (row,col)=>{
-        setSnake(snake.map((x,i)=> {
+        setBoard(board.map((x,i)=> {
             if(i==row)
             {
                 x[col]=x[col]?0:1;
@@ -19,14 +23,15 @@ const GameBox = ()=>{
         }));
     }
 
+
     return (
     <div className={Styles.GameBox}>
-        {[...Array(10)].map((x,row)=>
+        {[...Array(height)].map((x,row)=>
         (
             <div key={row} className={Styles.GameBoxColumn}>
-                {[...Array(10)].map((y,col)=> 
+                {[...Array(width)].map((y,col)=> 
                 (
-                    <Cell onClick={function(){handleClick(row,col)}} rown={row} coln={col} activated={snake[row][col]}key={col}/>
+                    <Cell onClick={function(){handleClick(row,col)}} rown={row} coln={col} activated={board[row][col]}key={col}/>
                 ))}
             </div>
         ))}
