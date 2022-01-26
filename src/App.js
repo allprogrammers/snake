37ba2ce365aa480e738e3d6c58f0c84dd1e03d42
Muhammad Hamza Ali <hamzaali44@gmail.com>
2022-01-26 21:52:56 +0100

    Removed boiler code further

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import GameBox from "./Components/GameBox";
+import React from 'react';
 
 function App() {
   return (
-    <GameBox />
+      <GameBox />
   );
 }
 
